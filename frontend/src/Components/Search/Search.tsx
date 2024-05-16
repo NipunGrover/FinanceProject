46dd5ddc49497bc7a9859dcb7574742633b25546
NipunGrover <ngrover5702@conestogac.on.ca>
2024-05-16 18:02:00 -0400

    added type checking to event handlers in search component

--- frontend/src/Components/Search/Search.tsx
+++ frontend/src/Components/Search/Search.tsx
@@ -3,19 +3,26 @@ import {useState} from 'react'
 type Props = {}
 
 const Search : React.FC<Props> = (props: Props) : JSX.Element => {
-    const[search, setSearch] = useState<string>("");
+  const[search, setSearch] = useState<string>("");
 
-    const onClick = (e: any) => {
-        setSearch(e.target.value);
-        
-    }
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setSearch(e.target.value); 
+  }
+
+  //Mouse Events can be hard to deal with in react, in that case you can use 
+  //the e:SyntheticEvent, which is a broader version which covers events but also provides 
+  //type checking (you might have to import the SyntheticEvent from react) 
+  // const onClick = (e:SyntheticEvent) => {}
+  const onClick = (e:React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    console.log(e);
+  }
 
   return (
-    <div>
-        <input value={search} onChange={(e) => onClick(e)}>
-        </input>
-    </div>
-  )
+      <div>
+          <input value={search} onChange={(e) => handleChange(e)}></input>
+          <button onClick={(e) => onClick(e)} />
+      </div>
+    )
 }
 
 export default Search
